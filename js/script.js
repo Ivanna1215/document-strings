ddd7980547dff9f810536002df61934359482237
Ivanna_1109 <ivanna.kovaliv1215@gmail.com>
2022-01-02 19:58:53 +0300

    commit1

---
+++ js/script.js
@@ -0,0 +1,49 @@
+'use strict'
+
+function guestList() {
+    let guestList = `Guests :
+    Ivanna
+    Ira
+    Mary
+    `;
+    console.log (guestList);
+}
+
+function getAllSymbol () {
+    for (let newString of 'IwillSucceed') {
+console.log (newString);
+    }
+}
+
+function СhangeTheCase () {
+   
+    console.log ('Interface'.toUpperCase());
+}
+function someSum (a,b){
+    return a + b;
+
+}
+console.log (`Sum: ${someSum(4,7)}`);
+function someString () {
+    let text = 'Hello!'
+    console.log (text.length);
+    let firstSymbol = text[0];
+    console.log (firstSymbol);
+    let lastSymbol = text[5];
+    console.log (lastSymbol);
+    lastSymbol = text [4];
+    console.log (lastSymbol);
+    lastSymbol = text [2];
+console.log (lastSymbol);
+lastSymbol =text [text.length-1];
+console.log (lastSymbol);
+for (const num of text) {
+    console.log (num);
+}
+console.log (text.toUpperCase());
+console.log (text.toLowerCase());
+console.log (text.toUpperCase());
+console.log (text.slice(1,4));
+
+
+} 
